01fb6a897232fc2241e52d50f879c4f2f7a3a9e7
pavan ofc <pavan2khanna@gmail.com>
2016-08-20 16:33:37 +0530

    few modifications are done in section3

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,29 @@
-  $(document).ready(function(){
-  // Add scrollspy to <body>
-  $('body').scrollspy({target: ".navbar", offset: 50});
-
-  // Add smooth scrolling on all links inside the navbar
+$(document).ready(function(){
+$('body').scrollspy({target: ".navbar", offset: 50});
   $("#myNavbar a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    }  // End if
+    } 
   });
+});
+
+$(document.getElementById("section1")).ready(function() {
+		// Activate Carousel
+		$("#slider").carousel({
+			interval : 3000,
+			pause : "hover"
+		});
+		// Enable Carousel Controls
+		/*$(".left").click(function() {
+			$("#slider").carousel("prev");
+		});
+		$(".right").click(function() {
+			$("#slider").carousel("next");
+		});*/
 });
